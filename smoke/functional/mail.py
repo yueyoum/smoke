758d31355e9b97a54d9e8d413f8fe7400e58498b
yueyoum <yueyoum@gmail.com>
2013-06-09 14:29:44 +0800

    adjsut

--- smoke/functional/mail.py
+++ smoke/functional/mail.py
@@ -3,9 +3,9 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def send_mail(host, port, username, password, mail_from, mail_to, mail_subject, mail_content, mail_type):
+def send_mail(host, port, username, password, mail_from, mail_to, mail_subject, mail_content, mail_type, display_from=None):
     content = MIMEText(mail_content, mail_type)
-    content['From'] = mail_from
+    content['From'] = display_from or mail_from
     if isinstance(mail_to, (list, tuple)):
         content['To'] = ', '.join(mail_to)
     else:
